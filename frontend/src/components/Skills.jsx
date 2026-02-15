9bf4a1b74e585b0640d494db4fc97dc28f603a50
Soham Jana <sohamjana2k25@gmail.com>
2026-02-15 14:07:51 +0530

    Create Skills.jsx

---
+++ frontend/src/components/Skills.jsx
@@ -0,0 +1,9 @@
+function Skills() {
+  return (
+    <section id="skills" className="p-20">
+      <h2 className="text-3xl font-bold mb-6">Skills</h2>
+      <p>React, Java, Spring Boot, Tailwind, MySQL, Python </p>
+    </section>
+  );
+}
+export default Skills;
