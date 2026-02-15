a915fa7b71743dce7ab495553ae1fdda0ba5119b
Soham Jana <sohamjana2k25@gmail.com>
2026-02-15 14:05:57 +0530

    Create About.jsx

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,37 @@
+function About() {
+  return (
+    <section id="about" className="min-h-screen flex items-center justify-center p-10">
+      <div className="bg-white/10 p-10 rounded-xl backdrop-blur-lg">
+            <h1 className="text-4xl font-bold mb-6">About Me</h1>
+            <p>Hello There!!!. I am a persuing CSE from Techno MAains Salt Lake. Currently exploring the fields of Machine Learning, Deep Learning and learning Java and Back-end Development. I have built and deployed one simple machine learning project from scratch. 
+
+            Occassionally I take  part in competitive programming contests on platforms like codeforces and codechef. Solved 100+ cp and dsa questions mostly in python. 
+            
+            Coding Portfolio -
+            ✅Codeforces - Pupil (max- 1201)
+            ✅Codechef - 2⭐ (max- 1457)
+            ✅Leetcode- 1540
+            ✅Hacker Rank- 2 ⭐ (python)
+            
+            Tech Stacks- 
+            
+            🌿Programing Languages:  Python , Java (Learning)
+            
+            🌿 Development - HTML5, CSS3, Flask ( Learning) , SpringBoot (Learning), SQL
+            
+            🌿AIML & Data Analysis - Numpy , Pandas, Matplotlib , Seaborn , Scikit-Learn , Regression, Predictions.
+            
+            Whats Next ?
+            
+            > Learn SpringBoot, Hibernate, JDBC, PostgreSQL and related tech stacks for java back-end development.
+            
+            > Improve DSA and problem solving skills enhancing coding Portfolio.
+            
+            > Explore Deep Learning and Agentic AI technologies like Tensorflow , spAcy, LangGraph, Llamaindex etc.
+            
+            > Participate in Hackathons and contribution to open source projects.</p>
+      </div>
+    </section>
+  );
+}
+export default About;
