5a912a44ea27fd4114ead2a73dc1c3e92367c7e4
Soham Jana <sohamjana2k25@gmail.com>
2026-02-15 14:10:03 +0530

    Create Education.jsx

---
+++ frontend/src/components/Education.jsx
@@ -0,0 +1,10 @@
+function Education() {
+  return (
+    <section id="education" className="p-20">
+      <h2 className="text-3xl font-bold mb-6">Education</h2>
+      <p>B.Tech in Computer Science @TMSL (2025-29)</p>
+      <p>PreSchool - Higher Secondary @TIGPS,Garia (2010-25)</p >
+    </section>
+  );
+}
+export default Education;
